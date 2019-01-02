d7c7df6de0094b128d4d663295250c2fd29e917f
caporta <caporta@users.noreply.github.com>
2019-01-01 21:44:01 -0500

    Add review read write and error handling

--- main.js
+++ main.js
@@ -31,16 +31,33 @@ Vue.component('product', {
             class="color-box"
             :style="{ backgroundColor: variant.variantColor }"
             @mouseover="updateProduct(index)"
-          ></div>
-
-          <button
-            v-on:click="addToCart"
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }"
           >
-            Add to Cart
-          </button>
+          </div>
+
+          <div>
+            <button
+              v-on:click="addToCart"
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }"
+            >
+              Add to Cart
+            </button>
+          </div>
+
+          <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+              <li v-for="review in reviews">
+                <p>{{ review.name }}</p>
+                <p>Rating: {{ review.rating }}</p>
+                <p>{{ review.review }}</p>
+              </li>
+            </ul>
+          </div>
         </div>
+
+        <product-review @review-submitted="addReview"/>
       </div>
   `,
   data: function() {
@@ -63,6 +80,7 @@ Vue.component('product', {
           variantQuantity: 0,
         },
       ],
+      reviews: [],
     }
   },
   methods: {
@@ -71,7 +89,10 @@ Vue.component('product', {
     },
     updateProduct: function(index) {
       this.selectedVariant = index
-    }
+    },
+    addReview: function(productReview) {
+      this.reviews.push(productReview)
+    },
   },
   computed: {
     title() {
@@ -89,6 +110,72 @@ Vue.component('product', {
       }
       return 2.99
     }
+  },
+})
+
+Vue.component('product-review', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):<b/>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name"/>
+      </p>
+
+      <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit">
+      </p>
+    </form>
+  `,
+  data: function() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: [],
+    }
+  },
+  methods: {
+    onSubmit: function() {
+      this.errors = []
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
+      } else {
+        if (!this.name) { this.errors.push('Name required.') }
+        if (!this.review) { this.errors.push('Review required.') }
+        if (!this.rating) { this.errors.push('Rating required.') }
+      }
+    },
   },
 })
 
